4b6e29c360b35c02df92130a35e2d3fe10ca5f7e
Iago Sousa <iagosousa201486@gmail.com>
2024-04-03 21:38:58 -0300

    Finalizando os componentes da table

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,13 @@
 import nlwUniteIcon from '../assets/nlw-unite-icon.svg';
+import { NavLink } from './nav-link';
 
 export function Header() {
   return (
     <div className="flex items-center gap-5">
       <img src={nlwUniteIcon}/>
       <nav className="flex items-center gap-5">
-        <a href="" className="font-medium text-sm text-zinc-200">Eventos</a>
-        <a href="" className="font-medium text-sm text-zinc-200">Participantes</a>
+        <NavLink href='/eventos'>Eventos</NavLink>
+        <NavLink href='/participantes'>Participantes</NavLink>
       </nav>
     </div>
   )
