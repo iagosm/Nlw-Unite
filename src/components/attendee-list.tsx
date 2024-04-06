59315a8ccc38b9f8223504dce29ea2e9f03448b8
Iago Sousa <iagosousa201486@gmail.com>
2024-04-06 19:21:01 -0300

    Finalizando projeto Nlw Unite - Trilha React

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -21,11 +21,23 @@ dayjs.locale('pt-br')
  }
 
 export function AttendeeList() {
-  const [search, setSearch] = useState('');
-  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState(() => {
+    const url = new URL(window.location.toString()); 
+    if(url.searchParams.has('search')){ 
+      return url.searchParams.get('search') ?? '';
+    }
+    return '';
+  });
+  const [page, setPage] = useState(() => {
+    const url = new URL(window.location.toString()); 
+    if(url.searchParams.has('page')){ 
+      return Number(url.searchParams.get('page'));
+    }
+    return 1;
+  });
   const [total, setTotal] = useState(0);
   const [attendees, setAttendees] = useState<Attendee[]>([]);
-
+  
   const totalPage = Math.ceil(total / 10);
 
   useEffect(() => {
@@ -42,25 +54,39 @@ export function AttendeeList() {
       })
   }, [page, search])
 
+  function setCurrentSearch(search: string) {
+    const url = new URL(window.location.toString());
+    url.searchParams.set('search', search);
+    window.history.pushState({}, "", url);
+    setSearch(search);
+  }
+  
+  function setCurrentPage(page: number) {
+    const url = new URL(window.location.toString());
+    url.searchParams.set('page', String(page));
+    window.history.pushState({}, "", url);
+    setPage(page);
+  }
+
   function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
-    setSearch(event.target.value);
-    setPage(1);
+    setCurrentSearch(event.target.value);
+    setCurrentPage(1);
   }
 
   function goToNextPage() {
-    setPage(page + 1)
+    setCurrentPage(page + 1);
   }
 
   function goToPreviousPage() {
-    setPage(page - 1)
+    setCurrentPage(page - 1);
   }
 
   function goToFirstPage() {
-    setPage(1)
+    setCurrentPage(1);
   }
   
   function goToLastPage() {
-    setPage(totalPage)
+    setCurrentPage(totalPage);
   }
 
   return (
@@ -70,8 +96,8 @@ export function AttendeeList() {
         <div className="w-72 px-3 py-1.5 border border-white/10 rounded-lg  flex items-center gap-3">
           <Search className='size-4 text-emerald-300' />
           <input 
-
           onChange={onSearchInputChanged} 
+          value={search}
           className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0"  type="text" placeholder="Buscar participante"/> 
         </div>
       </div>
@@ -154,4 +180,3 @@ export function AttendeeList() {
     </div>
   )
 }
-// 26
